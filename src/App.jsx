b1458be9eb3ba41c76475e5d93e3ae4a3f02fb17
Jesse de Koe <jdekoe@logictrade.nl>
2024-12-25 17:35:53 +0100

    contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import {LandingPage} from './pages/landingspage.jsx';
 import {AboutPage} from './pages/about.jsx';
+import {Contact} from "./pages/contact.jsx";
 import {CVPage} from './pages/cv.jsx';
 import {Layout} from "./components/layout.jsx";
 
@@ -11,6 +12,7 @@ export function App() {
                 <Routes>
                     <Route path="/" element={<LandingPage/>}/>
                     <Route path="/about" element={<AboutPage/>}/>
+                    <Route path="/contact" element={<Contact />} />
                     <Route path="/cv" element={<CVPage/>}/>
                 </Routes>
             </Layout>
