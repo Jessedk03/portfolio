2c345256f6ba52387ed0df387b6d2fffc3e5f8a0
Jesse <Jessedekoe03@gmail.com>
2024-12-25 20:54:14 +0100

    removed the contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {Routes, Route} from 'react-router-dom';
 import {LandingPage} from './pages/landingspage.jsx';
 import {AboutPage} from './pages/about.jsx';
-import {Contact} from "./pages/contact.jsx";
 import {CVPage} from './pages/cv.jsx';
 import {Layout} from "./components/layout.jsx";
 
@@ -12,7 +11,6 @@ export function App() {
                 <Routes>
                     <Route path="/" element={<LandingPage/>}/>
                     <Route path="/about" element={<AboutPage/>}/>
-                    <Route path="/contact" element={<Contact />} />
                     <Route path="/cv" element={<CVPage/>}/>
                 </Routes>
             </Layout>
